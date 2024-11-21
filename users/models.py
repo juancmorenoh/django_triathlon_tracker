d07854205489976d44913a748fccf1fb1a44da5c
juancmorenoh <camilo.mro@gmail.com>
2024-11-21 16:06:08 +0000

    Add Profile model and migrations

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    #OneToOneField = One Profile can be associated only to one User
+    #on_delete=models.CASCADE = If User is deleted also delete their Profile
+    #But not the other way around
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg',upload_to='profile_pics')
+
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,3 @@ from django.contrib import admin
 from . import views
 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),   
-]
