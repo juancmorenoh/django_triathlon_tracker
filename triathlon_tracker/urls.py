1d2c8b56392559f342197bd44335b2ea71905ad2
juancmorenoh <camilo.mro@gmail.com>
2024-11-24 09:49:52 +0000

    Added signals to handle cration of Profile along with User creation

--- triathlon_tracker/urls.py
+++ triathlon_tracker/urls.py
@@ -19,6 +19,10 @@ from django.contrib import admin
 from django.urls import include, path
 from users import views as user_views
 
+#To handle static elements (only debug=true)
+from django.conf import settings 
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('workout_logs.urls')), # Include the workout_logs app URLs
@@ -26,5 +30,8 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name ='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name ='users/logout.html'), name='logout'),
-    
 ]
+
+if settings.DEBUG:         
+   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
