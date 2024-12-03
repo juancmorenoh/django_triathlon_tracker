71b00ab43d57ce01c5cf3b56dea098eb0555c5be
juancmorenoh <camilo.mro@gmail.com>
2024-12-03 10:31:13 +0000

    Add functionality to update username,email,image from profile page

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,26 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm 
 from django.contrib.auth.models import User
+from .models import Profile
 
-
-
-class userRegistrationForm(UserCreationForm):
+class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
     class Meta:
         model = User
         fields = ['username','email', 'password1', 'password2',]
 
 
+
+#form to update useranem and password
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+
+#form to update img
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/views.py
+++ users/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import render , redirect, get_object_or_404
 from django.contrib.auth.models import User
-from .forms import userRegistrationForm
+from .forms import UserRegistrationForm, ProfileUpdateForm, UserUpdateForm
 from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
-        form = userRegistrationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('workout_list')  # Redirect to your homepage or any other page
     else:
-        form = userRegistrationForm()
+        form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
 
     
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    user_form =  UserUpdateForm(instance=request.user)
+    profile_form = ProfileUpdateForm(instance=request.user.profile)
+    
+    #You pass to the template a dictionary called context
+    #Left value is the name you will use in the template
+    #right value is what you are actually passing (The variable created in THIS view)
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+    return render(request, 'users/profile.html', context)
