764ef3ac7faa41ab7735f6525b92fa15f5858a0b
juancmorenoh <camilo.mro@gmail.com>
2024-12-03 11:46:48 +0000

    Override save method of profile model to resize saved pictures

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     #OneToOneField = One Profile can be associated only to one User
@@ -11,3 +12,13 @@ class Profile(models.Model):
     def __str__(self):
     
         return f"{self.user.username} Profile"
+    
+    def save(self):
+        super().save() #Understand why we calling the parent class method
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- users/views.py
+++ users/views.py
@@ -18,8 +18,18 @@ def register(request):
     
 @login_required
 def profile(request):
-    user_form =  UserUpdateForm(instance=request.user)
-    profile_form = ProfileUpdateForm(instance=request.user.profile)
+    if request.method == 'POST':
+        user_form =  UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST,
+                                        request.FILES,
+                                        instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('profile')
+    else:
+        user_form =  UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
     
     #You pass to the template a dictionary called context
     #Left value is the name you will use in the template
