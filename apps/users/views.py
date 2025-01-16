afef4604603f316d9fd34165d0dbdf3db9390276
juan camilo <camilo.mro@gmail.com>
2025-01-16 18:49:07 +0000

    Add workout statistic t profile page -Create separate statistics for each sport -Statistics are divided in - last 4 weeks, longest, all time and yearly -Yearly statistics can be filtered by year, separatly

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render , redirect, get_object_or_404
+from django.shortcuts import render , redirect
 from django.contrib.auth import login
 from django.contrib.auth.models import User
 from .forms import UserRegistrationForm, ProfileUpdateForm, UserUpdateForm
 from django.contrib.auth.decorators import login_required
 
+from apps.workout_logs.models import Workout
+from datetime import timedelta, datetime
+from django.db.models import Avg, Max, Sum, Count
+from django.utils.timezone import localtime
 
 # Create your views here.
 
@@ -40,7 +44,70 @@ def update_profile(request):
     }
     return render(request, 'users/update_profile.html', context)
 
-
+@login_required
 def profile(request):
-    context = {}
+    user = request.user
+    current_date = datetime.now()
+    #
+    current_year = current_date.year
+    four_weeks_ago = current_date - timedelta(weeks=4)
+
+    activity_types = ['run', 'ride', 'swim']
+    stats = {}
+
+    #
+    range_selected_years = range(current_year - 5, current_year + 1)
+    
+    for activity in activity_types:
+        #
+        selected_year = int(request.GET.get(f'year_{activity}', current_year))
+        workouts = Workout.objects.filter(user=user, activity_type=activity)
+
+
+        
+
+        last_4_weeks = workouts.filter(date__gte=four_weeks_ago) #any date >= 4weeks ago
+        last_4_weeks_count = last_4_weeks.count()
+        avg_distance_last_4_weeks = last_4_weeks.aggregate(Avg('distance_m'))['distance_m__avg'] or 0#if avd distance is None, set it to 0
+        avg_time_last_4_weeks = last_4_weeks.aggregate(Avg('duration'))['duration__avg'] or 0
+
+        longest_activity = workouts.aggregate(Max('distance_m'), Max('duration'))
+        longest_distance = longest_activity['distance_m__max'] or 0
+        longest_time = longest_activity['duration__max'] or 0
+
+
+        total_distance = workouts.aggregate(Sum('distance_m'))['distance_m__sum'] or 0
+        total_time = workouts.aggregate(Sum('duration'))['duration__sum'] or 0
+        total_activities = workouts.count()
+
+        #
+        yearly_workouts = workouts.filter(date__year=selected_year)
+        yearly_count = yearly_workouts.count()
+        yearly_distance = yearly_workouts.aggregate(Sum('distance_m'))['distance_m__sum'] or 0
+        yearly_time = yearly_workouts.aggregate(Sum('duration'))['duration__sum'] or 0
+
+
+        stats[activity] = {
+            'last_4_weeks_count': last_4_weeks_count,
+            'avg_distance_last_4_weeks': avg_distance_last_4_weeks / 1000, 
+            'avg_time_last_4_weeks': avg_time_last_4_weeks,
+            'longest_distance': longest_distance / 1000, 
+            'longest_time': longest_time,
+            'total_distance': total_distance / 1000,
+            'total_time': total_time,
+            'total_activities': total_activities,
+
+            #
+            'yearly_count': yearly_count,
+            'yearly_distance': yearly_distance / 1000,  # Convert to km
+            'yearly_time': yearly_time,
+            'selected_year': selected_year
+        }
+
+    
+    context = {
+        'stats': stats,
+        'range_selected_years': range_selected_years,
+    }
+
     return render(request, 'users/profile.html', context)
