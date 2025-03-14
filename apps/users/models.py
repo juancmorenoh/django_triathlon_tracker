5198298947a3630f7d70c6bc331b2bb79f218133
juan camilo <camilo.mro@gmail.com>
2025-03-14 13:50:17 +0000

    -Modify save function of Profile model to handle the img and set to default if null(null added for the field)

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,27 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+import os
+from django.conf import settings
 
 class Profile(models.Model):
     #OneToOneField = One Profile can be associated only to one User
     #on_delete=models.CASCADE = If User is deleted also delete their Profile
     #But not the other way around
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg',upload_to='profile_pics')
+    image = models.ImageField(default='default.jpg',  null=True)
 
     def __str__(self):
     
         return f"{self.user.username} Profile"
 
     #understand why the args and kwargs are being passed here
-    def save(self, *args, **kwargs):  # Add *args and **kwargs here
-        super().save(*args, **kwargs)  # Pass *args and **kwargs to the parent class's save method
+    def save(self, *args, **kwargs):
+        # Ensure default is used if image is None
+        if not self.image:
+            self.image = 'default.jpg'
 
-        img = Image.open(self.image.path)
+        super().save(*args, **kwargs)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300,300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+        # Resize image if not default
+        if self.image and self.image.name != 'default.jpg':
+            img = Image.open(self.image.path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
     
     
