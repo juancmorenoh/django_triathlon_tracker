41059930b8dd08ddaa2fcd4d09f0a5cfb42fa231
juan camilo <camilo.mro@gmail.com>
2025-03-14 23:21:29 +0000

    -add authentication for the rest framework

--- triathlon_tracker/settings.py
+++ triathlon_tracker/settings.py
@@ -36,6 +36,8 @@ ALLOWED_HOSTS = ["*"]
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
