0bb4833b7015efe90551c1eb598468957212609f
juan camilo <camilo.mro@gmail.com>
2025-03-16 20:33:35 +0000

    Add api urls: refresh, Create User

--- triathlon_tracker/settings.py
+++ triathlon_tracker/settings.py
@@ -36,8 +36,8 @@ ALLOWED_HOSTS = ["*"]
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ),
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
@@ -67,8 +67,6 @@ INSTALLED_APPS = [
     'apps.api',
     'apps.frontend',
 ]
-
-
 
 
 MIDDLEWARE = [
--- triathlon_tracker/urls.py
+++ triathlon_tracker/urls.py
@@ -18,13 +18,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from apps.workout_logs import views as workout_logs_views
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from apps.api.views import UserCreate
 #To handle static elements (only debug=true)
 from django.conf import settings 
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/users/register', UserCreate.as_view(), name='register'),
+    path('api/token/', TokenObtainPairView.as_view(), name='get_toker'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('api-auth/', include('rest_framework.urls')),
+
     path('', workout_logs_views.home, name='home'),
     
 
