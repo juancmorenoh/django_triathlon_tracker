648c48eaf78d3e66c24b9cfab54ee5a1e8bfa66b
juan camilo <camilo.mro@gmail.com>
2025-03-16 21:44:20 +0000

    Change BASE_DIR of templates back to "templates"

--- triathlon_tracker/settings.py
+++ triathlon_tracker/settings.py
@@ -85,7 +85,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'apps/frontend/templates',  
+            BASE_DIR / 'templates',  
         ],
         'APP_DIRS': True, #find app-specific tenplates from any installed app
         'OPTIONS': {
