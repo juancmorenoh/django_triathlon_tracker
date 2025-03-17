95d0eaab327ec19bd48978b426e05c90893aa208
juan camilo <camilo.mro@gmail.com>
2025-03-17 23:41:50 +0000

    Add WorkoutDetail and UserCreate to the urls in api app

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import WorkoutListCreate,WorkoutDelete
+from .views import WorkoutListCreate,WorkoutDelete,UserCreate,WorkoutDetail
 
 urlpatterns = [
     path('workouts/', WorkoutListCreate.as_view(), name = 'workouts-list'),
+    path('workouts/<int:pk>', WorkoutDetail.as_view(), name = 'workouts-detail'),
     path('workouts/delete/<int:pk>', WorkoutDelete.as_view(), name='workout-delete'),
+    path('user/register', UserCreate.as_view(), name='register'),  # Added this new URL path.
 ]
