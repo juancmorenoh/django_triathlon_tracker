3446c3f4d4770bdffc70f65e12e45b902f2ff498
juan camilo <camilo.mro@gmail.com>
2025-03-17 15:14:34 +0000

    Removed dotenv from settings.py

--- triathlon_tracker/settings.py
+++ triathlon_tracker/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
-from dotenv import load_dotenv
+
 #credentials
-load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
