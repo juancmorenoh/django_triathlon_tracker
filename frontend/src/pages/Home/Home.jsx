be2e0fd875edd8d0f1aa81e04a219887b1f5f143
juan camilo <camilo.mro@gmail.com>
2025-03-17 23:40:56 +0000

    Create Home component -Simple homepage with very basic authentication -Accessible by all users -dinamically display links to login/logout.register

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,29 @@
+import {Link} from 'react-router-dom';
+import styles from './Home.module.css';
+
 function Home(){
+  /* Improve authentication, this is basic 
+  check expiration time or api call to check*/
+  const isAuthenticated = Boolean(localStorage.getItem('access'));
+
   return(
-    <div>
-      Home
-    </div>
+    <>
+      <section className={styles.intro}>
+        <div>
+          <h1>Welcome to Your Triathlon Tracker!</h1>
+        </div>
+        {!isAuthenticated &&(
+          <div className={styles.linkContainer}>
+            <Link to='/login'>Login</Link>
+            <Link to='/register'>Register</Link>
+          </div>
+        )}
+        <div>
+          <h2>Your Triathlon Training</h2>
+          <p>Track your progress and monitor your workouts across three key disciplines:running, swimming, and cycling.</p>
+        </div>
+      </section>
+    </>
   )
 }
 
