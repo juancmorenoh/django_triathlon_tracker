ce8628112470cecd1a90a350ce3e858fe2f38ce8
juan camilo <camilo.mro@gmail.com>
2025-03-17 23:51:30 +0000

    -add NavLink to workouts list in navbar -add classname to divs(2) -implement incorrect authentication(commented out)

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,28 +1,42 @@
 import styles from './NavBar.module.css';
-import {Link} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import {Link, NavLink} from 'react-router-dom';
 
 function NavBar(){
+  /* const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    const token = Boolean(localStorage.getItem('access'));;
+    setIsAuthenticated(token);
+  }, []); */
+
   return(
     <nav className={styles.navbar}>
-      <div>
+      <div className={styles.titleContainer}>
         <Link to="/home" className={styles.title}>Triathlon Tracker</Link>
       </div>
 
       <div className={styles.menu}>
         <ul className={styles.navLinks}>
-          <li><a href="#">Profile</a></li>
-          <li><a href="#">Workouts</a></li>
+          <li><NavLink href="#">Profile</NavLink></li>
+          <li><NavLink to="/workouts">Workouts</NavLink></li>
           <li><a href="#">Races</a></li>
           <li><a href="#">Goals</a></li>
         </ul>
       </div>
 
-      <div>
+      <div className={styles.rightActionButtons}>
+        <Link to="/logout">Logout</Link>
         <Link to="/login">Login</Link>
         <Link to="/register">Register</Link>
-        <Link to="/logout">Logout</Link>
+        {/* {isAuthenticated ? (
+          <Link to="/logout">Logout</Link>
+        ):(
+          <>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+          </>
+        )} */}
       </div>
-      
     </nav>
   )
 }
