7ea49633c1f0a3ce44af0ad60aedbd03682e8378
juan camilo <camilo.mro@gmail.com>
2025-03-17 15:15:43 +0000

    Implement Routes in App.jsx -Create Logout and RegisterAndLogout components in App -Add routes to:  home,login,logout and register in Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import {BrowserRouter,Routes, Route, NavLink, Navigate} from 'react-router-dom'
+import NavBar from './components/NavBar/NavBar';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+
+import ProtectedRoute from './components/ProtectedRoute';
+
+function Logout(){
+  localStorage.clear();
+  return <Navigate to='/login' />
+}
+
+function RegisterAndLogout(){
+  localStorage.clear();
+  return <Register />
+}
 
 function App() {
   return (
     <>
-      
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path='/home' element={<ProtectedRoute><Home /></ProtectedRoute>}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/logout' element={<Logout />}></Route>
+        <Route path='/register' element={<RegisterAndLogout />}></Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
