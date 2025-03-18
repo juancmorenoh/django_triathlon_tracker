71dd3823601104c63c0d5b11eba8683b4b69374c
juan camilo <camilo.mro@gmail.com>
2025-03-18 02:05:45 +0000

    Remove api app - user app updates: -Update url in app users to reflect changes -Add serializer to user app -Create OldView.py to store the old implementations(you never know)

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from . import views as user_views
-from django.contrib.auth import views as auth_views
+
+from .views import UserCreate
+# from . import views as user_views
+# from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('register/', user_views.register, name='register'),
-    path('profile/', user_views.profile, name='profile'),
-    path('profile/update', user_views.update_profile, name='profile_update'),
-    path('login/', auth_views.LoginView.as_view(template_name ='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name ='users/logout.html'), name='logout'),
+    path('create/', UserCreate.as_view(), name='user-create')
+
+    # path('register/', user_views.register, name='register'),
+    # path('profile/', user_views.profile, name='profile'),
+    # path('profile/update', user_views.update_profile, name='profile_update'),
+    # path('login/', auth_views.LoginView.as_view(template_name ='users/login.html'), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(template_name ='users/logout.html'), name='logout'),
 ]
