267a7923b64ec6d76699fc55f085e176b8d302f7
juan camilo <camilo.mro@gmail.com>
2025-03-18 12:27:39 +0000

    Add Discipline Serializer -Add disciplines for RaceSerializer -exclude user from fields in Goal-Race serializers

--- backend/tracker/serializer.py
+++ backend/tracker/serializer.py
@@ -1,19 +1,32 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Workout,Race,Goal;
+from .models import Workout,Race,Goal, Discipline
     
-class WorkoutSerializer(serializers.ModelSerializer):
+class WorkoutSerializer(serializers.HyperlinkedModelSerializer):
+  #HyperlinkedModelSerializer tries to create URLs for related fields (user ForeignKey).
+  #So need read_only to fix the problem since no user-detail url present
+  user = serializers.PrimaryKeyRelatedField(read_only=True)
   class Meta:
     model = Workout
-    # remove user as it's set automatically with method perform_create
     fields = "__all__"
+    read_only_fields = ['user']
 
-class RaceSerializer(serializers.ModelSerializer):
+class DisciplineSerializer(serializers.HyperlinkedModelSerializer):
+  race = serializers.HyperlinkedRelatedField(read_only=True, view_name='race-detail')
   class Meta:
-    model = Race
-    fields = "__all__"
+    model = Discipline
+    fields = '__all__'
+
+class RaceSerializer(serializers.HyperlinkedModelSerializer):
+    disciplines = DisciplineSerializer(many=True, read_only=True)
+    class Meta:
+      model = Race
+      exclude = ['user']
+    
 
 class GoalSerializer(serializers.ModelSerializer):
   class Meta:
     model = Goal
-    fields = "__all__"
+    exclude = ['user']
+
+
