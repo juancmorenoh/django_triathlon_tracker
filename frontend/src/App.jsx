36256cc9e8b7b4fa3465508de00c1b98ed817409
juan camilo <camilo.mro@gmail.com>
2025-03-24 20:21:54 +0000

    Groupe routes into a separate Component called AppRoutes.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,42 +2,20 @@ import React from 'react'
 import {BrowserRouter,Routes, Route, NavLink, Navigate} from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
-import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
-import Workouts from './pages/Workouts/Workouts';
-import WorkoutDetails from './components/WorkoutDetails/WorkoutDetails';
-import Races from './pages/Races/Races';
-import Goals from './pages/Goals/Goals';
-import ProtectedRoute from './components/ProtectedRoute';
-import Profile from './pages/Profile/Profile';
 
-function Logout(){
-  localStorage.clear();
-  return <Navigate to='/login' />
-}
+import AppRoutes from './components/AppRoutes.jsx';
+import { useState, useEffect } from 'react';
+import ProtectRoute from './components/ProtectedRoute.jsx';
 
-function RegisterAndLogout(){
-  localStorage.clear();
-  return <Register />
-}
 
 function App() {
+
+
   return (
     <>
     <BrowserRouter>
       <NavBar />
-      <Routes>
-        <Route path='/home' element={<Home />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/logout' element={<Logout />}></Route>
-        <Route path='/register' element={<RegisterAndLogout />}></Route>
-        <Route path='/workouts' element={<ProtectedRoute><Workouts /></ProtectedRoute>}></Route>
-        <Route path='/workouts/:id' element={<ProtectedRoute><WorkoutDetails /></ProtectedRoute>}></Route>
-        <Route path='/races' element={<ProtectedRoute><Races /></ProtectedRoute>}></Route>
-        <Route path='/goals' element={<ProtectedRoute><Goals /></ProtectedRoute>}></Route>
-        <Route path='/profile' element={<ProtectedRoute><Profile /></ProtectedRoute>}></Route>
-      </Routes>
+      <AppRoutes />
       <Footer />
     </BrowserRouter>
     </>
---
+++ frontend/src/components/AppRoutes.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import ProtectedRoute from './ProtectedRoute.jsx';
+import WorkoutDetails from './WorkoutDetails/WorkoutDetails.jsx';
+import Profile from './../pages/Profile/Profile.jsx';
+import Workouts from './../pages/Workouts/Workouts.jsx';
+import Home from './../pages/Home/Home.jsx';
+import Login from './../pages/Login/Login.jsx';
+import Goals from './../pages/Goals/Goals.jsx';
+import Races from './../pages/Races/Races.jsx';
+import Register from './../pages/Register/Register.jsx';
+
+function AppRoutes() {
+
+  // Logout function
+  function Logout() {
+    localStorage.clear();
+    return <Navigate to='/login' />
+  }
+
+  // Register and logout function
+  function RegisterAndLogout() {
+    localStorage.clear();
+    return <Register />
+  }
+
+  return (
+    <Routes>
+      {/* Public Routes */}
+      <Route path='/home' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/logout' element={<Logout />} />
+      <Route path='/register' element={<RegisterAndLogout />} />
+
+      {/* Protected Routes */}
+      <Route path='/workouts' element={<ProtectedRoute><Workouts /></ProtectedRoute>} />
+      <Route path='/workouts/:id' element={<ProtectedRoute><WorkoutDetails /></ProtectedRoute>} />
+      <Route path='/races' element={<ProtectedRoute><Races /></ProtectedRoute>} />
+      <Route path='/goals' element={<ProtectedRoute><Goals /></ProtectedRoute>} />
+      <Route path='/profile' element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        
+
+      {/* Catch-all Route */}
+      <Route path="*" element={<Navigate to="/home" />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
