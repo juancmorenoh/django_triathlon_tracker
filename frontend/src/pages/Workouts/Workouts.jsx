52d36408d06b766cca05999e8b0701988ae2c496
juan camilo <camilo.mro@gmail.com>
2025-03-25 16:56:05 +0000

    Add workout details state in Workouts.jsx to pass to single workouts -create function to create Workout(api post request) -add function to toogle display of Workoutdetails.jsx, pass to row in table -Create a section for the workout form -create section to display workout details

--- frontend/src/pages/Workouts/Workouts.jsx
+++ frontend/src/pages/Workouts/Workouts.jsx
@@ -2,27 +2,68 @@ import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import styles from './Workouts.module.css';
 import api from "../../api"
+import WorkoutForm from './../../components/Forms/WorkoutForm.jsx';
+import WorkoutDetails from './../../components/Workout/WorkoutDetails.jsx';
 
 function Workouts() {
   const [workouts, setWorkouts] = useState([]);
+
+  //Workoutform state
+  const [activity_type, setActivityType] = useState('');
+  const [date, setDate] = useState('');
+  const [distance_m, setDistance] = useState('');
+  const [duration, setDuration] = useState('');
+  const [name, setName] = useState('');
+  const [intensity, setIntensity] = useState('');
+  const [notes, setNotes] = useState('');
+
+  const[isWorkoutOpen, setIsWorkoutOpen] = useState(false)
+  const[selectedWorkout, setSelectedWorkout] = useState(null)
 
   useEffect(() => {
     getWorkouts();
   }, []);
 
+  //get all workouts from api
   const getWorkouts = () =>{
     //Fetch data from api
     api
-    .get('/tracker/workouts')
+    .get('/tracker/workouts/')
     .then((res) => res.data)
     .then((data) => {setWorkouts(data); console.log(data);})
-    .catch((err) => alert(err));
+    .catch((error) => alert(error));
   }
+
+  //save workout form in database
+  const createWorkout = (e) => {
+    e.preventDefault();
+    api
+    .post('/tracker/workouts/', {activity_type, date, distance_m, duration, name, intensity, notes})
+    .then((res) => {
+      console.log(res.data)
+      if (res.status == 201) alert('Success')
+      else (alert('Error'))
+      
+    })
+    .catch((error) => alert(error))
+  }
+  
+  //Function to toggle the view of the workout
+  function handleWorkoutClick(workout){
+    //if selectedWorkout not null, if not null compare it to workout.id
+    if (selectedWorkout?.id === workout.id) {
+      setIsWorkoutOpen((prev) => !prev);
+    } else {
+      setSelectedWorkout(workout);
+      setIsWorkoutOpen(true);
+    }
+  }
+
   return (
+    <>
     <div className={styles.listContainer}>
       <div className={styles.introList}>
         <h2>Workout List</h2>
-        <Link className={styles.addElementLink} to="/workouts/new">Add New Workout</Link>
       </div>
       <div className={styles.tableContainer}>
           
@@ -39,12 +80,13 @@ function Workouts() {
           </thead>
           <tbody>
             {workouts.length > 0 ? (
-              workouts.map((workout) => (
-                <tr key={workout.id}>
-                  <td>
-                    <Link to={`/workouts/${workout.id}`}>
-                      {workout.activity_type}
-                    </Link>
+              workouts.map((workout) => ( 
+                <>
+                <tr key={workout.id} 
+                    onClick={()=>handleWorkoutClick(workout)}>
+                      {console.log(isWorkoutOpen)}
+                  <td>               
+                    {workout.activity_type}   
                   </td>
                   <td>{workout.date}</td>
                   <td>{workout.distance_m} m</td>
@@ -52,6 +94,8 @@ function Workouts() {
                   <td>{workout.name}</td>
                   <td>{workout.intensity}</td>
                 </tr>
+                </>
+      
               ))
             ) : (
               <tr>
@@ -62,6 +106,29 @@ function Workouts() {
         </table>
       </div>
     </div>
+    {/* Section to create a new Workout */}
+    <section className={styles.workout}>
+      <WorkoutForm 
+        createWorkout={createWorkout}
+        setActivityType={setActivityType} 
+        setDate={setDate}
+        setDistance={setDistance}
+        setDuration={setDuration}
+        setName={setName}
+        setIntensity={setIntensity}
+        setNotes={setNotes}
+      />
+    </section>
+
+    {/* section to see details of workout (DELETE/UPDATE) */}
+    {isWorkoutOpen &&(
+      <section>
+        <WorkoutDetails workout={selectedWorkout}></WorkoutDetails>
+      </section>
+    )}
+    
+    </>
+    
     
   );
 }
